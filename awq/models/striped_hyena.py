4f2981929b126bdf635aed812d7ee1056bf5f7d0
Casper Hansen <casperbh.96@gmail.com>
2023-12-23 19:56:27 +0000

    Striped hyena initial

---
+++ awq/models/striped_hyena.py
@@ -0,0 +1,61 @@
+from .base import BaseAWQForCausalLM
+
+class StripedHyenaAWQForCausalLM(BaseAWQForCausalLM):
+    layer_type = "blocks"
+    max_new_tokens_key = "max_seqlen"
+
+    @staticmethod
+    def get_model_layers(model):
+        return model.backbone.blocks
+    
+    @staticmethod
+    def get_act_for_scaling(module):
+        return dict(
+            is_scalable=False
+        )
+    
+    @staticmethod
+    def move_embed(model, device: str):
+        model.backbone.embedding_layer = model.backbone.embedding_layer.to(device)
+        model.backbone.unembed = model.backbone.unembed.to(device)
+    
+    @staticmethod
+    def get_layers_for_scaling(module, input_feat, module_kwargs):
+        layers = []
+
+        if module.__class__.__name__ == "ParallelGatedConvBlock":
+            layers.append(dict(
+                prev_op=module.pre_norm,
+                layers=[module.projections, module.out_filter_dense],
+                inp=input_feat['projections'],
+                module2inspect=module.projections,
+                kwargs=module_kwargs,
+            ))
+        elif module.__class__.__name__ == "AttentionBlock":
+            layers.append(dict(
+                prev_op=module.pre_norm,
+                layers=[module.inner_mha_cls.Wqkv],
+                inp=input_feat['inner_mha_cls.Wqkv'],
+                module2inspect=module.inner_mha_cls,
+                kwargs=module_kwargs,
+            ))
+            layers.append(dict(
+                prev_op=module.inner_mha_cls.Wqkv,
+                layers=[module.inner_mha_cls.out_proj],
+                inp=input_feat['inner_mha_cls.out_proj'],
+            ))
+        
+        layers.append(dict(
+            prev_op=module.post_norm,
+            layers=[module.mlp.l1, module.mlp.l2],
+            inp=input_feat[f'mlp.l1'],
+            module2inspect=module.mlp,
+            kwargs=module_kwargs,
+        ))
+        layers.append(dict(
+            prev_op=module.mlp.l2,
+            layers=[module.mlp.l3],
+            inp=input_feat[f'mlp.l3'],
+        ))
+
+        return layers
---
+++ examples/striped_hyena_quant.py
@@ -0,0 +1,22 @@
+from awq import AutoAWQForCausalLM
+from transformers import AutoTokenizer
+
+model_path = 'togethercomputer/StripedHyena-Nous-7B'
+quant_path = 'striped-hyna-nous-awq'
+quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
+
+# Load model
+# NOTE: pass safetensors=True to load safetensors
+model = AutoAWQForCausalLM.from_pretrained(
+    model_path, safetensors=True, **{"low_cpu_mem_usage": True, "use_cache": False, "use_flash_rmsnorm": False}
+)
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+
+# Quantize
+model.quantize(tokenizer, quant_config=quant_config)
+
+# Save quantized model
+model.save_quantized(quant_path)
+tokenizer.save_pretrained(quant_path)
+
+print(f'Model is quantized and saved at "{quant_path}"')
